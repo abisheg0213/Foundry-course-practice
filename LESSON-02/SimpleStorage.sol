47b2fa97044ca33ffc8bd5dc74e096378835786e
abisheg0213 <109604992+abisheg0213@users.noreply.github.com>
2023-07-11 15:04:28 +0530

    Create SimpleStorage.sol

---
+++ LESSON-02/SimpleStorage.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Indentifier : MIT
+pragma solidity ^0.8.14;
+contract SimpleStorage
+{
+    uint public favoriteNumber;
+    struct person
+    {
+        uint  favNumber;
+        string name;
+    }
+    mapping (string => uint )favnumperson;
+    person[] public  myfriends;
+    function store(uint _favoriteNumber) public
+    {
+        favoriteNumber=_favoriteNumber;
+    }
+    function addfriend(string memory name,uint favnum) public
+    {
+        myfriends.push(person(favnum,name));
+        favnumperson[name]=favnum;
+    }
+    function getfavnum(string memory name) view public returns (uint)
+    {
+        return favnumperson[name];
+    }
+}
