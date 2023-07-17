b7775d61f0e8b949e9b474779a678e5323305640
abisheg0213 <109604992+abisheg0213@users.noreply.github.com>
2023-07-17 11:58:02 +0530

    Update FundMe.sol

--- LESSON-04/FundMe.sol
+++ LESSON-04/FundMe.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.8.14;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract FundMe
 {
+    address [] public funders;
+    mapping (address =>uint) amountfunded;
     address payable owner;
     constructor()
     {
@@ -10,6 +12,8 @@ contract FundMe
     uint public minimum_usd=5;
     function fund() public payable {
         require(msg.value >= getconversion(),"Required amount not given");
+        funders.push(msg.sender);
+        amountfunded[msg.sender]+=msg.value;
     }
     function withdraw() public{
         uint balance=address(this).balance;
